b0127a4dd425a4d049a4d33ccce4b9fb765d710f
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-08 08:31:06 -0500

    Added: Comments about using Glue.

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 package;
 
-import example1.Example1;
 import glue.Glue;
+import example1.Example_1;
 import openfl.display.Sprite;
 
 /**
@@ -16,16 +16,37 @@ class Main extends Sprite
 		super();
 
 		/**
-		 * All assets in one loading process. 
+		 *  If is it necessary, you can describe all the assets in one array
+		 *  before the game starts. Then you pass all the data to the start() method.
+		 *  
+		 *  Syntax:
+		 *  
+		 *  assets:Array<Any> = [
+		 *  	{
+		 *  		type	:	[ "image" | "spritesheet" | "data" ],
+		 *  		src		:	[ Path of the content. ],
+		 *  		id		:	[ Name by which the asset will be invoked from the code. ]
+		 *  	}];
 		 */
 		var assets:Array<Any> = [
-			{ type:"image", src:"img/background_game.png", id:"background_game" }
-			// { type:"image", src:"img/floor.png", id:"floor" },
-			// { type:"sprite", src:"img/player_idle", id:"player_idle" }
+			{ type:"image", src:"img/background_game.png", id:"background_game" },
+			{ type:"image", src:"img/floor.png", id:"floor" },
+			{ type:"spritesheet", src:"img/player_idle.png", id:"player_idle" }
 		];
 
+		/**
+		 *  Starts the game engine.
+		 *  
+		 *  Syntax:
+		 *  
+		 *  Glue.start({
+		 *  	mainScene (required)	:	[ First Scene from where the engine will start. ],
+		 *  	assets (optional)			:	[ The assets array declared beore. ],
+		 *  	isDebug (optional)		: [ It will show traces in debugging mode. ]
+		 *  });
+		 */
 		Glue.start({
-			mainScene: Example1,
+			mainScene: Example_1,
 			assets: assets,
 			isDebug: true
 		});
