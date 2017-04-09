b7bb04cd95e0c0c93545d50ef4d7e74d3ae34efb
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-09 09:44:31 -0500

    Added getClassName().

--- src/glue/utils/GTools.hx
+++ src/glue/utils/GTools.hx
@@ -29,4 +29,12 @@ package glue.utils;
 			return str.substr(0, str.length - prec) + '.' + str.substr(str.length - prec);
 		}
 	}
+
+	public static function getClassName(object:Any):String
+	{
+		if (object == null) return null;
+		var a:Array<String> = cast(Type.getClassName(Type.getClass(object)), String).split(".");
+		var className:String = a[a.length - 1];
+		return className;
+	}
 }
