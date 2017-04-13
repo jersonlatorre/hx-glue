3f77a5a38e3d780e5ac222db2c5608a9d48bf241
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-13 09:07:30 -0500

    Added: Background and button.

--- Example-Character/src/scenes/MenuScene.hx
+++ Example-Character/src/scenes/MenuScene.hx
@@ -1,7 +1,9 @@
 package scenes;
 
+import glue.Glue;
 import glue.ui.GScene;
 import glue.display.GImage;
+import glue.display.GButton;
 
 /**
  * ...
@@ -11,20 +13,25 @@ import glue.display.GImage;
 
 class MenuScene extends GScene
 {
-	var _background:GImage;
-	
 	override public function preload()
 	{
-		load({ type:"image", src:"img/game_background.png", id:"game_background" });
+		load({ type:"image", src:"img/menu_background.png", id:"menu_background" });
 		load({ type:"button", src:"img/button_play.png", id:"button_play" });
 	}
 
 	override public function init()
 	{
-		gotoScene(GameScene);
-	}
-	
-	override public function update()
-	{
+		new GImage("menu_background").addTo(this);
+
+		new GButton("button_play")
+			.addTo(this)
+			.setAnchor(0.5, 0.5)
+			.setPosition(Glue.width / 2, Glue.height / 2)
+			.onClick(function()
+			{
+				gotoScene(GameScene);
+			});
+
+		fadeIn();
 	}
 }
