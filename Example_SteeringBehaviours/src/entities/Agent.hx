eb9bc37dcd9aee551bde8d4c2dd9f5eb502d9297
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-13 15:59:45 -0500

    New example.

---
+++ Example_SteeringBehaviours/src/entities/Agent.hx
@@ -0,0 +1,47 @@
+package entities;
+
+import glue.Glue;
+import glue.utils.GTime;
+import glue.display.GEntity;
+import glue.math.GVector2D;
+
+/**
+ * ...
+ * @author Jerson La Torre
+ */
+
+class Agent extends GEntity
+{
+	var _target:Target;
+
+	var _velocity:GVector2D = new GVector2D();
+	var _desired:GVector2D = new GVector2D();
+	var _steer:GVector2D = new GVector2D();
+
+	static inline var MAX_SPEED:Float = 150;
+	static inline var MAX_FORCE:Float = 100;
+
+	override public function init()
+	{
+		createRectangleGraphic(30, 15, 0x0000FF);
+		setAnchor(0.5, 0.5);
+		setPosition(Glue.width / 2, Glue.height / 2);
+	}
+
+	override public function update()
+	{
+		_desired = (_target.position - position).scaledTo(MAX_SPEED);
+		_steer = (_desired - _velocity).truncate(MAX_FORCE);
+
+		_velocity += _steer * GTime.deltaTime;
+		_velocity.truncate(MAX_SPEED);
+		position += _velocity * GTime.deltaTime;
+
+		setRotation(_velocity.angle());
+	}
+
+	public function setTarget(target:Target)
+	{
+		_target = target;
+	}
+}
---
+++ Example_SteeringBehaviours/src/entities/Target.hx
@@ -0,0 +1,23 @@
+package entities;
+
+import glue.input.GMouse;
+import glue.display.GEntity;
+
+/**
+ * ...
+ * @author Jerson La Torre
+ */
+
+class Target extends GEntity
+{
+	override public function init()
+	{
+		createCircleGraphic(10, 0xFF0000);
+		setAnchor(0.5, 0.5);
+	}
+
+	override public function update()
+	{
+		position = GMouse.position;
+	}
+}
