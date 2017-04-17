7913d9052637434ecd7b646a19fe3e1c83c40057
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-17 07:34:07 -0500

    Added: sounds!

--- Example-CharacterWalk/src/scenes/GameScene.hx
+++ Example-CharacterWalk/src/scenes/GameScene.hx
@@ -1,5 +1,6 @@
 package scenes;
 
+import glue.input.GMouse;
 import glue.display.GImage;
 import entities.Player;
 import glue.scene.GScene;
@@ -20,18 +21,32 @@ class GameScene extends GScene
 		loadSpritesheet("player_idle", "images/player_idle.png");
 		loadSpritesheet("player_walk", "images/player_walk.png");
 		loadSpritesheet("player_jump", "images/player_jump.png");
+		#if (html5 || cpp)
+		loadSound("bgm_game", "sounds/bgm_game.ogg");
+		loadSound("jump", "sounds/jump.ogg");
+		#else
+		loadSound("bgm_game", "sounds/bgm_game.mp3");
+		loadSound("jump", "sounds/jump.mp3");
+		#end
 	}
 
 	override public function init()
 	{
+		loopSound("bgm_game");
+
 		new GImage("game_background").addTo(this);
 		
 		_player = new Player().addTo(this);
+
 		fadeIn();
 	}
 	
 	override public function update()
 	{
-		
+		if (GMouse.isDown)
+		{
+			gotoScene(MenuScene);
+			stopAllSounds();
+		}
 	}
 }
