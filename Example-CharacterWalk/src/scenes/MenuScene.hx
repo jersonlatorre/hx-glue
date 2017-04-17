dd6e1c6c02f0a9383829a75dad2ab194323afc06
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-17 07:34:25 -0500

    Added: Sounds!

--- Example-CharacterWalk/src/scenes/MenuScene.hx
+++ Example-CharacterWalk/src/scenes/MenuScene.hx
@@ -4,6 +4,8 @@ import glue.Glue;
 import glue.scene.GScene;
 import glue.display.GImage;
 import glue.display.GButton;
+import openfl.media.Sound;
+import openfl.media.SoundChannel;
 
 /**
  * ...
@@ -13,24 +15,48 @@ import glue.display.GButton;
 
 class MenuScene extends GScene
 {
+	var sound:Sound;
+	var channel:SoundChannel;
+	
 	override public function preload()
 	{
 		loadImage("menu_background", "images/menu_background.png");
 		loadButton("button_play", "images/button_play.png");
+
+		#if (html5 || cpp)
+		loadSound("button_over", "sounds/button_over.ogg", "fx");
+		loadSound("button_down", "sounds/button_down.ogg", "fx");
+		loadSound("bgm_menu", "sounds/bgm_menu.ogg", "music");
+		#else
+		loadSound("button_over", "sounds/button_over.mp3", "fx");
+		loadSound("button_down", "sounds/button_down.mp3", "fx");
+		loadSound("bgm_menu", "sounds/bgm_menu.mp3", "music");
+		#end
 	}
 
 	override public function init()
 	{
+		// loopSound("bgm_menu");
+
 		new GImage("menu_background").addTo(this);
 
 		new GButton("button_play")
-		.addTo(this)
-		.setAnchor(0.5, 0.5)
-		.setPosition(Glue.width / 2, Glue.height / 2)
-		.onClick(function()
-		{
-			gotoScene(GameScene);
-		});
+			.addTo(this)
+			.setAnchor(0.5, 0.5)
+			.setPosition(Glue.width / 2, Glue.height / 2)
+			.onMouseEnter(function()
+			{
+				playSound("button_over");
+			})
+			.onMouseDown(function()
+			{
+				playSound("button_down");
+			})
+			.onClick(function()
+			{
+				stopSound("bgm_menu");
+				gotoScene(GameScene);
+			});
 
 		fadeIn();
 	}
