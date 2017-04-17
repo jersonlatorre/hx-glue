cf646e146e4bca4135d0491229423698e3792bbb
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-17 17:27:14 -0500

    New GKeyboard class.

--- src/glue/input/GKeyboard.hx
+++ src/glue/input/GKeyboard.hx
@@ -9,70 +9,111 @@ import openfl.ui.Keyboard;
  * @author Jerson La Torre
  */
 
+ enum GKeyState { NONE; UP; DOWN; JUST_PRESSED; }
+
 @:final class GKeyboard 
 {
-	static public inline var LEFT:Int = 37;
-	static public inline var RIGHT:Int = 39;
-	static public inline var UP:Int = 38;
-	static public inline var DOWN:Int = 40;
-	static public inline var SPACE:Int = 32;
-	static public inline var PAUSE:Int = 80;
-	
-	static public var isLeft:Bool = false;
-	static public var isRight:Bool = false;
-	static public var isUp:Bool = false;
-	static public var isDown:Bool = false;
-	static public var isSpace:Bool = false;
-	static public var isPause:Bool = false;
-	
-	public function new() 
-	{
-		
-	}
-	
-	static public function pressed(name:Dynamic)
-	{
-		if (Std.is(name, String))
-		{
-			
-		}
-		
-		if (Std.is(name, Int))
-		{
-			
-		}
-	}
-	
+	static var _actions:Map<String, Array<Int>> = new Map<String, Array<Int>>();
+	static var _keys:Map<Int, GKeyState> = new Map<Int, GKeyState>();
+
 	static public function init()
 	{
 		Glue.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
 		Glue.stage.addEventListener(KeyboardEvent.KEY_UP, onKeyUp);
-		
 	}
-	
-	static function onKeyDown(e:KeyboardEvent) 
+
+	static public function bind(actionName:String, keys:Array<Int>)
 	{
-		switch (e.keyCode)
+		_actions.set(actionName, keys);
+	}
+
+	static public function isDown(actionName:String):Bool
+	{
+		if (!_actions.exists(actionName))
 		{
-			case LEFT, Keyboard.A: isLeft = true;
-			case RIGHT, Keyboard.D: isRight = true;
-			case UP, Keyboard.W: isUp = true;
-			case DOWN, Keyboard.S: isDown = true;
-			case SPACE: isSpace = true;
-			case PAUSE: isPause = true;
+			throw 'The action \'$actionName\' has not been binded.';
+			return false;
 		}
-	}
-	
-	static function onKeyUp(e:KeyboardEvent) 
-	{
-		switch (e.keyCode)
+
+		for (key in _actions.get(actionName))
 		{
-			case LEFT, Keyboard.A: isLeft = false;
-			case RIGHT, Keyboard.D: isRight = false;
-			case UP, Keyboard.W: isUp = false;
-			case DOWN, Keyboard.S: isDown = false;
-			case SPACE: isSpace = false;
-			case PAUSE: isPause = false;
+			if (_keys.exists(key) &&
+					(_keys.get(key) == GKeyState.DOWN) || (_keys.get(key) == GKeyState.JUST_PRESSED))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	static public function justPressed(actionName:String):Bool
+	{
+		if (!_actions.exists(actionName))
+		{
+			throw 'The action \'$actionName\' has not been binded.';
+			return false;
+		}
+
+		for (key in _actions.get(actionName))
+		{
+			if (_keys.exists(key) &&
+					_keys.get(key) == GKeyState.JUST_PRESSED)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	static public function isUp(actionName:String):Bool
+	{
+		if (!_actions.exists(actionName))
+		{
+			throw 'The action \'$actionName\' has not been binded.';
+			return null;
+		}
+
+		for (key in _actions.get(actionName))
+		{
+			if (_keys.exists(key) &&
+					_keys.get(key) == GKeyState.UP)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	static function onKeyDown(e:KeyboardEvent)
+	{
+		if (_keys.get(e.keyCode) != GKeyState.DOWN)
+			_keys.set(e.keyCode, GKeyState.JUST_PRESSED);	
+	}
+
+	static function onKeyUp(e:KeyboardEvent)
+	{
+		_keys.set(e.keyCode, GKeyState.UP);
+	}
+
+	static public function update()
+	{
+		for (action in _actions)
+		{
+			for (key in action)
+			{
+				if (_keys.get(key) == GKeyState.UP)
+				{
+					_keys.set(key, GKeyState.NONE);
+				}
+
+				if (_keys.get(key) == GKeyState.JUST_PRESSED)
+				{
+					_keys.set(key, GKeyState.DOWN);
+				}
+			}
 		}
 	}
 }
