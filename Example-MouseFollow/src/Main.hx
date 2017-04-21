d9467071c2d14785b2202e0c730d5af29a0fae25
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-21 05:25:14 -0500

    Testing bitmapfonts.

--- Example-MouseFollow/src/Main.hx
+++ Example-MouseFollow/src/Main.hx
@@ -21,6 +21,9 @@ class Main extends Sprite
 
 		 Glue.loadImage("background_game", "images/background_game.png");
 		 Glue.loadImage("floor", "images/floor.png");
+		 Glue.loadImage("font_bitmap", "fonts/font.png");
+		 Glue.loadData("font_data", "fonts/font.fnt");
+		 Glue.loadImage("font_monospace", "fonts/monospace.png");
 		 Glue.loadSpritesheet("character_idle", "images/character_idle.png");
 
 		/**
@@ -31,8 +34,10 @@ class Main extends Sprite
 		 *  Glue.start({
 		 *  	mainScene (required)	:	[ First Scene from where the engine will start. ],
 		 *  	isDebug (optional)		: [ It will show traces in debugging mode. ]
+		 *  	showStats (optional)	: [ It will show FPS and memory stats. ]
 		 *  });
 		 */
+
 		Glue.start({
 			mainScene: GameScene,
 			isDebug: true,
