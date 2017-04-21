fb354a145db8fee794f284c3facc9c2a7e2b2630
Jerson La Torre <jerson.latorre@gmail.com>
2017-04-21 05:30:34 -0500

    Testing better looking stats.

--- src/glue/utils/GStats.hx
+++ src/glue/utils/GStats.hx
@@ -1,22 +1,35 @@
 package glue.utils;
 	
 import haxe.Timer;
+import glue.display.GBitmapText;
 import openfl.events.Event;
 import openfl.system.System;
-import openfl.text.TextField;
-import openfl.text.TextFormat;
 import openfl.display.Sprite;
+import openfl.display.Shape;
 
 @:final class GStats extends Sprite
 {
-	private var textFPS:TextField;
-	private var textMEM:TextField;
-	private var textPeak:TextField;
-	private var background:Sprite;
-	private var times:Array<Float>;
-	private var memoryPeak:Float = 0;
+	private var labelFPS:GBitmapText;
+	private var textFPS:GBitmapText;
+	private var labelMEM:GBitmapText;
+	private var textMEM:GBitmapText;
 
-	private var graph:Sprite;
+	private var background:Shape;
+	private var graph:Shape;
+
+	private var times:Array<Float>;
+	private var fps:Int;
+	private var systemFPS:Int;
+	private var memoryPeak:Float = 0;
+	private var memory:Float = 0;
+
+	static var STATS_WIDTH:Int;
+	static var STATS_HEIGHT:Int;
+	static inline var BAR_WIDTH:Int = 150;
+	static inline var BAR_HEIGHT:Int = 14;
+	static inline var MARGIN:Int = 2;
+	static inline var TEXT_WIDTH:Int = 40;
+	static inline var FONT_SIZE:Float = 2;
 
 	public function new(x:Int = 0, y:Int = 0)
 	{
@@ -25,133 +38,83 @@ import openfl.display.Sprite;
 		this.x = x;
 		this.y = y;
 
-		graph = new Sprite();
+		systemFPS = Std.int(Glue.stage.frameRate);
 
-		textFPS = new TextField();
-		textFPS.x = 2;
-		textFPS.y = -2;
-		textFPS.selectable = false;
-		textFPS.defaultTextFormat = new TextFormat("Arial", 9, 0xFFFFFF);
-		textFPS.text = "FPS";
+		STATS_WIDTH = TEXT_WIDTH + BAR_WIDTH + MARGIN;
+		STATS_HEIGHT = 2 * BAR_HEIGHT + 3 * MARGIN;
+		
+		labelFPS = new GBitmapText();
+		labelFPS.position.x = 4;
+		labelFPS.position.y = 4;
+		labelFPS.size = FONT_SIZE;
+		labelFPS.preUpdate();
 
-		textMEM = new TextField();
-		textMEM.x = 2;
-		textMEM.y = 10;
-		textMEM.selectable = false;
-		textMEM.defaultTextFormat = new TextFormat("Arial", 9, 0xFFFFFF);
-		textMEM.text = "Mem";
+		labelMEM = new GBitmapText();
+		labelMEM.position.x = 4;
+		labelMEM.position.y = 20;
+		labelMEM.size = FONT_SIZE;
+		labelMEM.preUpdate();
 
-		textPeak = new TextField();
-		textPeak.x = 2;
-		textPeak.y = 22;
-		textPeak.selectable = false;
-		textPeak.defaultTextFormat = new TextFormat("Arial", 9, 0xFFFFFF);
-		textPeak.text = "Peak";
+		textFPS = new GBitmapText();
+		textFPS.position.x = TEXT_WIDTH + 4;
+		textFPS.position.y = 4;
+		textFPS.size = FONT_SIZE;
+		textFPS.preUpdate();
 
-		background = new Sprite();
-		background.graphics.beginFill(0x000000, 0.6);
-		background.graphics.drawRect(0, 0, 91, 37);
+		textMEM = new GBitmapText();
+		textMEM.position.x = TEXT_WIDTH + 4;
+		textMEM.position.y = 20;
+		textMEM.size = FONT_SIZE;
+		textMEM.preUpdate();
+
+		labelFPS.text = 'FPS';
+		labelMEM.text = 'MEM';
+
+		background = new Shape();
+		background.graphics.beginFill(0x000000, 0.55);
+		background.graphics.drawRect(0, 0, STATS_WIDTH, STATS_HEIGHT);
 		background.graphics.endFill();
+
+		graph = new Shape();
 
 		addChild(background);
 		addChild(graph);
-		addChild(textFPS);
-		addChild(textMEM);
-		addChild(textPeak);
+		
+		labelFPS.addToLayer(this);
+		labelMEM.addToLayer(this);
+		textFPS.addToLayer(this);
+		textMEM.addToLayer(this);
 		
 		times = [];
 		
-		addEventListener(Event.ENTER_FRAME, onEnter);
+		addEventListener(Event.ENTER_FRAME, update);
 	}
 	
-	private function onEnter(e:Event)
-	{	
+	private inline function update(e:Event)
+	{
 		var now = Timer.stamp();
 		times.push(now);
 		
 		while (times[0] < now - 1) times.shift();
+
+		fps = if (times.length > systemFPS) systemFPS else times.length;
 			
-		var memory:Float = Math.round(System.totalMemory / 1024 / 1024 * 100) / 100;
+		memory = Std.int(System.totalMemory >> 20);
 		if (memory > memoryPeak) memoryPeak = memory;
+
+		textFPS.text = '${ fps }/${ systemFPS }';
+		textMEM.text = '${ memory } Mb/${ memoryPeak } Mb';
 		
 		graph.graphics.clear();
-		graph.graphics.beginFill(0x00CC55);
-		graph.graphics.drawRect(30, 1, if (times.length > 60) 60 else times.length , 11);
+		graph.graphics.beginFill(0xAAAAAA);
+		graph.graphics.drawRect(TEXT_WIDTH, MARGIN, BAR_WIDTH, BAR_HEIGHT);
+		graph.graphics.beginFill(0xAAAAAA);
+		graph.graphics.drawRect(TEXT_WIDTH, BAR_HEIGHT + (MARGIN << 1), BAR_WIDTH, BAR_HEIGHT);
+		graph.graphics.beginFill(0x119922);
+		graph.graphics.drawRect(TEXT_WIDTH, MARGIN, BAR_WIDTH * fps / systemFPS , BAR_HEIGHT);
+		graph.graphics.beginFill(0xEE4400);
+		graph.graphics.drawRect(TEXT_WIDTH, BAR_HEIGHT + (MARGIN << 1), BAR_WIDTH * memory / if(memoryPeak == 0) 1 else memoryPeak, BAR_HEIGHT);
 		graph.graphics.endFill();
 
-		graph.graphics.beginFill(0xFFCC00);
-		graph.graphics.drawRect(30, 13, 60, 11);
-		graph.graphics.endFill();
-
-		graph.graphics.beginFill(0xFF5500);
-		graph.graphics.drawRect(30, 25, 60, 11);
-		graph.graphics.endFill();
-
-		if (visible)
-		{	
-			// text.htmlText = '<b><font color="#FFFF00">FPS: </font></b>${ times.length }\n<b><font color="#FFFF00">MEM: </font></b>${ memory } MB\n<b><font color="#FFFF00">MEM peak: </font></b>${ memoryPeak } MB';
-		}
 	}
 }
-
-
-// package glue.utils;
-	
-// import haxe.Timer;
-// import openfl.events.Event;
-// import openfl.system.System;
-// import openfl.text.TextField;
-// import openfl.text.TextFormat;
-// import openfl.display.Sprite;
-
-// @:final class GStats extends Sprite
-// {
-// 	private var text:TextField;
-// 	private var background:Sprite;
-// 	private var times:Array<Float>;
-// 	private var memoryPeak:Float = 0;
-
-// 	public function new(x:Int = 0, y:Int = 0, color:Int = 0xFFFFFF)
-// 	{
-// 		super();
-		
-// 		this.x = x;
-// 		this.y = y;
-
-// 		text = new TextField();
-// 		text.width = 155;
-// 		text.height = 63;
-// 		text.x = 10;
-// 		text.y = 8;
-// 		text.selectable = false;
-// 		text.defaultTextFormat = new TextFormat("Verdana", 11, color);
-
-// 		background = new Sprite();
-// 		background.graphics.beginFill(0x000000, 0.6);
-// 		background.graphics.drawRect(0, 0, 155, 63);
-// 		background.graphics.endFill();
-
-// 		addChild(background);
-// 		addChild(text);
-		
-// 		times = [];
-		
-// 		addEventListener(Event.ENTER_FRAME, onEnter);
-// 	}
-	
-// 	private function onEnter(e:Event)
-// 	{	
-// 		var now = Timer.stamp();
-// 		times.push(now);
-		
-// 		while (times[0] < now - 1) times.shift();
-			
-// 		var memory:Float = Math.round(System.totalMemory / 1024 / 1024 * 100) / 100;
-// 		if (memory > memoryPeak) memoryPeak = memory;
-		
-// 		if (visible)
-// 		{	
-// 			text.htmlText = '<b><font color="#FFFF00">FPS: </font></b>${ times.length }\n<b><font color="#FFFF00">MEM: </font></b>${ memory } MB\n<b><font color="#FFFF00">MEM peak: </font></b>${ memoryPeak } MB';
-// 		}
-// 	}
-// }
