7d129fc9649fdbfb9b379a46791679646f2a5b59
Jerson La Torre <jerson.latorre@gmail.com>
2017-10-13 10:13:54 -0500

    Removed functions for create prototype graphics.

--- src/glue/display/GImage.hx
+++ src/glue/display/GImage.hx
@@ -2,7 +2,7 @@ package glue.display;
 
 import glue.assets.GLoader;
 import openfl.display.Bitmap;
-import openfl.display.Shape;
+import openfl.display.PixelSnapping;
 
 /**
  * ...
@@ -14,49 +14,21 @@ class GImage extends GEntity
 {
 	var _image:Bitmap = null;
 	
-	public function new()
+	public function new(assetId:String)
 	{
 		super();
-	}
-
-	public function createFromAsset(assetId:String)
-	{
-		_image = GLoader.getImage(assetId);
-		_skin.addChild(_image);
-		this.width = _skin.width;
-		this.height = _skin.height;
-	}
-
-	override public function createRectangleShape(width:Float, height:Float, color:UInt = 0x000000)
-	{
-		this.width = width;
-		this.height = height;
-
-		var graphic = new Shape();
-		graphic.graphics.beginFill(color);
-		graphic.graphics.drawRect(0, 0, width, height);
-		graphic.graphics.endFill();
-
-		_skin.addChild(graphic);
-	}
-
-	override public function createCircleShape(radius:Float, color:UInt = 0x000000)
-	{
-		this.width = this.height = 2 * radius;
-
-		var graphic = new Shape();
-		graphic.graphics.beginFill(color);
-		graphic.graphics.drawCircle(radius, radius, radius);
-		graphic.graphics.endFill();
-
-		_skin.addChild(graphic);
+		var image:Bitmap = new Bitmap(GLoader.getImage(assetId));
+		image.pixelSnapping = PixelSnapping.ALWAYS;
+		_skin.addChild(image);
+		width = _skin.width;
+		height = _skin.height;
+		bounds.setTo(_skin.x, _skin.y, width, height);
 	}
 
 	override public function destroy() 
 	{
-		_skin.removeChild(_image);
+		if (_skin != null) _skin.removeChild(_image);
 		super.destroy();
-		_image.bitmapData.dispose;
 		_image = null;
 	}
 }
