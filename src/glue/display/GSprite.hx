7b0a68c6cc982a39e0cd42c099a94a2c498843b9
Jerson La Torre <jerson.latorre@gmail.com>
2017-10-13 10:14:26 -0500

    Testing new ways to load frames.

--- src/glue/display/GSprite.hx
+++ src/glue/display/GSprite.hx
@@ -4,7 +4,10 @@ import glue.assets.GLoader;
 import glue.utils.GTime;
 import glue.utils.GTools;
 import openfl.display.Bitmap;
-import openfl.display.Sprite;
+import openfl.display.BitmapData;
+import openfl.display.Shape;
+import openfl.geom.Rectangle;
+import openfl.geom.Point;
 
 /**
  * ...
@@ -46,7 +49,6 @@ class GSprite extends GEntity
 		
 		_sprite = new __GSpriteBase(_animations[name].id, _animations[name].fps, _animations[name].loop);
 		_sprite.addToLayer(_skin);
-		_sprite.adjustPosition();
 		if (_onEndAnimation != null) _sprite.onEndAnimation(_onEndAnimation);
 		
 		width = _sprite.width;
@@ -65,6 +67,8 @@ class GSprite extends GEntity
 	{
 		if (_sprite != null) _sprite.preUpdate();
 		super.preUpdate();
+		// width = _sprite.width;
+		// height = _sprite.height;
 	}
 	
 	override public function destroy() 
@@ -77,56 +81,68 @@ class GSprite extends GEntity
 
 @:final class __GSpriteBase extends GEntity
 {
-	var _image:Bitmap;
-	var _mask:Sprite;
-	var _frames:Array<Dynamic> = new Array<Dynamic>();
 	var _framesLength:Int;
 	var _currentFrameIndex:Float = 0;
 	var _fps:Int;
 	var _loop:Bool;
+	var _frame:Bitmap;
+
+	// #if CREATE_ONE_ROW_SPRITESHEET
+	var _frames:BitmapData;
+	// #end
+
+	// #if CACHE_FRAME_PER_FRAME
+	// var _frames:Array<BitmapData>;
+	// #end
+
+	var _frameBmd:BitmapData;
+	var _mask:Shape;
+	var _drawRect:Rectangle;
+	var _drawPoint:Point;
 	var _isPaused:Bool = false;
 	var _onEndAnimation:Dynamic = null;
+	var rounded:Int = 0;
 	
-	public function new(spriteId:String, fps:Int = 30, loop:Bool = true) 
+	public function new(assetId:String, fps:Int = 30, loop:Bool = true)
 	{
 		super();
 
 		_fps = fps;
 		_loop = loop;
 
-		_frames = GLoader.getJson(spriteId + "_data").frames;
-		_framesLength = _frames.length;
-		
-		width = _frames[0].sourceSize.w * _scaleX;
-		height = _frames[0].sourceSize.h * _scaleY;
-		
-		_image = GLoader.getImage(spriteId);
-		_mask = new Sprite();
+		_frames = GLoader.getSpritesheet(assetId).frames;
+
+		_framesLength = GLoader.getSpritesheet(assetId).numFrames;
+		width = GLoader.getSpritesheet(assetId).width;
+		height = GLoader.getSpritesheet(assetId).height;
+
+		// #if CACHE_FRAME_PER_FRAME
+		// _drawRect = new Rectangle(0, 0, width, height);
+		// _drawPoint = new Point(0, 0);
+		// _frameBmd = new BitmapData(Std.int(width), Std.int(height), true, 0x00000000);
+		// _frame = new Bitmap(_frameBmd);
+		// _skin.addChild(_frame);
+		// #end
+
+		// #if CREATE_ONE_ROW_SPRITESHEET
+		_frame = new Bitmap(_frames);
+		_mask = new Shape();
 		_mask.graphics.beginFill(0);
 		_mask.graphics.drawRect(0, 0, width, height);
 		_mask.graphics.endFill();
-		_image.mask = _mask;
-		
-		_skin.addChild(_image);
+		_frame.mask = _mask;
+		_skin.addChild(_frame);
 		_skin.addChild(_mask);
+		// #end
 	}
 	
 	public function onEndAnimation(callback:Dynamic)
 	{
 		_onEndAnimation = callback;
 	}
-
-	@:allow(glue.display.GSprite.play)
-	function adjustPosition()
-	{
-		_image.x = -Std.int(_frames[0].frame.x);
-		_image.y = -Std.int(_frames[0].frame.y);
-	}
 	
 	override public function preUpdate()
 	{
-		if (_isPaused) return;
-
 		_currentFrameIndex += _fps * GTime.deltaTime;
 		
 		if(_currentFrameIndex >= _framesLength)
@@ -135,19 +151,23 @@ class GSprite extends GEntity
 			if (_onEndAnimation != null) _onEndAnimation();
 		}
 		
-		var rounded:Int = Std.int((_currentFrameIndex) % _framesLength);
-		_image.x = -Std.int(_frames[rounded].frame.x);
-		_image.y = -Std.int(_frames[rounded].frame.y);
+		rounded = Std.int((_currentFrameIndex) % _framesLength);
+		
+		// #if CACHE_FRAME_PER_FRAME
+		// _frameBmd.fillRect(_drawRect, 0);
+		// _frameBmd.draw(_frames[rounded]);
+		// #end
+
+		// #if CREATE_ONE_ROW_SPRITESHEET
+		if (_frame != null) _frame.x = -rounded * width;
+		// #end
 	}
 
 	override public function destroy()
 	{
-		_skin.removeChild(_image);
-		_skin.removeChild(_mask);
-		super.destroy();
-		_image.bitmapData.dispose();
-		_image.bitmapData.disposeImage();
-		_image = null;
+		_frame.bitmapData.disposeImage();
+		_frame = null;
 		_mask = null;
+		super.destroy();
 	}
 }
